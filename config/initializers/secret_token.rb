9320b8b4012aea414f8d39d2e1a8a73fd7cdece2
Max Plomer <maxplomer@gmail.com>
2014-11-12 12:53:59 -0500

    initial repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CodechefClone::Application.config.secret_key_base = '0a7826303b26aee1292eddbf355107ac3e73563b74d2e150754a18ad58c2e52164a1271f177a508bd6f8aae804b4a5e1820caa5b8817659922fbdcb88ced4d49'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CodechefClone::Application.config.session_store :cookie_store, key: '_codechef_clone_session'
